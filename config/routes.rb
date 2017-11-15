62de0a2b0e49e6a88122fd1a860cac711df7a923
Kimura Daichi <daichi703n@DiMac.designet.local>
2017-11-16 01:54:03 +0900

    Success Google OAuth Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   resources :links
   get 'feature_pages/manage'
 
